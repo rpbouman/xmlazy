7b5bf68ba3970a037552b55983140fa076e10ae7
rpbouman <roland.bouman@gmail.com>
2021-04-14 12:27:26 +0200

    Add namepsace methods (improve test coverage)

--- tests/dom/CDataSectionNode.test.js
+++ tests/dom/CDataSectionNode.test.js
@@ -4,17 +4,23 @@ describe('CData Section', () => {
 
   const text = 'world';
   const cdata = `<![CDATA[${text}]]>`;
+  const ns1 = 'urn.corp:tmp1';
+  const ns2 = 'urn.corp:tmp2';
+  const pfx = 'x';
   let 
     staxStringReader, staxResult, 
-    xml, elementNode, cdataSectionNode
+    xml, elementNode, cdataSectionNode, cdataSectionNode2
   ; 
   beforeAll(() => {
-    xml = `<hello>${cdata}</hello>`;
+    xml = `<hello>${cdata}<${pfx}:world xmlns="${ns1}" xmlns:${pfx}="${ns2}"><![CDATA[world]]></${pfx}:world></hello>`;
     staxStringReader = new xmlazy.StaxStringReader(xml, {chainNodes: true});
     staxResult = staxStringReader.next();
     elementNode = staxResult.value;
     staxResult = staxStringReader.next();
     cdataSectionNode = staxResult.value;
+    staxResult = staxStringReader.next();
+    staxResult = staxStringReader.next();
+    cdataSectionNode2 = staxResult.value
   });
 
   it('nodeType is 4', () => {
@@ -64,5 +70,26 @@ describe('CData Section', () => {
   it(`data must be ${text}`, () => {
     expect(cdataSectionNode.data).toBe(text);
   });
+  
+  describe('Namespace tests', () => {
+    it('can lookupPrefix', () => {
+      expect(cdataSectionNode2.lookupPrefix(ns2)).toBe(pfx);
+    });
 
+    it('can lookupNamespaceURI', () => {
+      expect(cdataSectionNode2.lookupNamespaceURI(pfx)).toBe(ns2);
+    });
+    
+    it('can check isDefaultNamespace when true', () => {
+      expect(cdataSectionNode.isDefaultNamespace('')).toBe(true);
+    });
+    
+    it('can check isDefaultNamespace when false', () => {
+      expect(cdataSectionNode2.isDefaultNamespace('')).toBe(false);
+    });
+
+    it('can check isDefaultNamespace when true and namespace is not empty', () => {
+      expect(cdataSectionNode2.isDefaultNamespace(ns1)).toBe(true);
+    });
+  });
 });
