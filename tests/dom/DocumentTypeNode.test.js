57c77e002593c4274db4ade6c8ce06f88e50d8a2
Roland Bouman <Roland.Bouman@gmail.com>
2024-06-30 13:24:36 +0200

    Added test for internal subset DOCTYPE

--- tests/dom/DocumentTypeNode.test.js
+++ tests/dom/DocumentTypeNode.test.js
@@ -2,7 +2,7 @@ import * as xmlazy from '../../src/xmlazy.js';
 
 describe('Document Type Node', () => {
 
-  describe("Basic doctype node tets", () => {
+  describe("Basic external DOCTYPE node tets", () => {
     const docTypeXml = '<!DOCTYPE greeting SYSTEM "hello.dtd">'; 
     const xml = `<?xml version="1.0"?>
       ${docTypeXml}
@@ -48,4 +48,63 @@ describe('Document Type Node', () => {
     });
     
   });
+  
+  describe("Basic DOCTYPE with internal subset", ()=>{
+    const docTypeXml = `<!DOCTYPE note
+[
+<!ELEMENT note (to,from,heading,body)>
+<!ELEMENT to (#PCDATA)>
+<!ELEMENT from (#PCDATA)>
+<!ELEMENT heading (#PCDATA)>
+<!ELEMENT body (#PCDATA)>
+]>`;
+    const xml = `<?xml version="1.0"?>
+      ${docTypeXml}
+      <note>
+        <to>Whom it may concern</to>
+        <from>Yours truly</from>
+        <heading>heading text</heading>
+        <body>body text</body>
+      </note>
+    `;
+    let staxStringReader, staxResult, documentNode, docTypeNode;
+    
+    beforeAll(() => {
+      staxStringReader = new xmlazy.StaxStringReader(xml);
+      documentNode = staxStringReader.buildDocument();
+      docTypeNode = documentNode.doctype;
+    });
+      
+    describe('Basic doc type node tests', () =>{
+      it('nodeType is 10', () => {
+        expect(docTypeNode.nodeType).toBe(10);
+      });
+
+      it('name is "greeting"', () => {
+        expect(docTypeNode.name).toBe("note");
+      });
+
+      it('publicId is ""', () => {
+        expect(docTypeNode.publicId).toBe("");
+      });
+
+      it('systemId is ""', () => {
+        expect(docTypeNode.systemId).toBe("");
+      });
+
+      it(`sourceXml is ${docTypeXml}`, () => {
+        expect(docTypeNode.sourceXml).toBe(docTypeXml);
+      });
+
+      it('hasChildNodes() is false', () => {
+        expect(docTypeNode.hasChildNodes()).toBe(false);
+      });
+
+      it('childNodes.length is 0', () => {
+        expect(docTypeNode.childNodes.length).toBe(0);
+      });
+    });
+    
+  });
+  
 });
